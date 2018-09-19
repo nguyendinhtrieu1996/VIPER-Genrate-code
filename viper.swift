5088847e4caa7a57f0266e55104c73a8225b0438
dinhtrieu <trieu.nguyen@bacsitoancau.com>
2018-09-19 15:46:45 +0700

    Update Service, input and output

--- viper.swift
+++ viper.swift
@@ -24,7 +24,7 @@ func getUserName(_ args: String...) -> String {
 }
 
 let userName = getUserName()
-let module = CommandLine.arguments[1].capitalized
+let module = CommandLine.arguments[1]
 let fileManager = FileManager.default
 
 //MARK: URL
@@ -36,12 +36,18 @@ let viewUrl           = moduleUrl.appendingPathComponent("View")
 let interactorUrl      = moduleUrl.appendingPathComponent("Interactor")
 let routerUrl         = moduleUrl.appendingPathComponent("Router")
 let presenterUrl      = moduleUrl.appendingPathComponent("Presenter")
+let commonURL         = moduleUrl.appendingPathComponent("Common")
+let serviceURL        = commonURL.appendingPathComponent("Service")
 
 let contractFileUrl   = contractUrl.appendingPathComponent(module+"Contract").appendingPathExtension("swift")
 let viewFileUrl   = viewUrl.appendingPathComponent(module+"ViewController").appendingPathExtension("swift")
 let interactorFileUrl   = interactorUrl.appendingPathComponent(module+"Interacter").appendingPathExtension("swift")
 let presenterFileUrl   = presenterUrl.appendingPathComponent(module+"Presenter").appendingPathExtension("swift")
 let routerFileUrl   = routerUrl.appendingPathComponent(module+"Router").appendingPathExtension("swift")
+
+//Service
+let endpointFileURL = serviceURL.appendingPathComponent(module+"Endpoint").appendingPathExtension("swift")
+let serviceFileURL = serviceURL.appendingPathComponent(module+"Service").appendingPathExtension("swift")
 
 //MARK: File comment
 func fileComment(for module: String, type: String) -> String {
@@ -69,13 +75,15 @@ let contractFileContent = """
 
 import UIKit
 
+//MARK: VIEW
 protocol \(module)View: class {
 
     var presenter: \(module)Presentation? { get set }
 
 }
 
-protocol \(module)Presentation: class {
+//MARK: PRESENTER
+protocol \(module)Presentation {
 
     var view: \(module)View? { get set }
     var router: \(module)WireFrame? { get set }
@@ -83,16 +91,32 @@ protocol \(module)Presentation: class {
 
 }
 
+//MARK: PRESENTER
 protocol \(module)UseCase: class {
-
+    var service: \(module)ServiceInputProtocol? { get set }
+    var output: \(module)InteractorOutputProtocol? { get set }
 
 }
 
-protocol \(module)WireFrame: class {
+protocol \(module)InteractorOutputProtocol: BaseInteracterOutputProtocol {
+
+}
+
+//MARK: ROUTER
+protocol \(module)WireFrame: BaseRouterProtocol {
 
     var viewController: UIViewController? { get set }
 
     static func assembleModule() -> UIViewController
+
+}
+
+//MARK: SERVIVE
+protocol \(module)ServiceInputProtocol: class {
+    var requestHandler: \(module)ServiceOutputProtocol? { get set }
+}
+
+protocol \(module)ServiceOutputProtocol: BaseServiceOutputProtocol {
 
 }
 
@@ -132,7 +156,28 @@ let interactorFileContent = """
 
 import Foundation
 
-class \(module)Interacter: \(module)UseCase {
+class \(module)Interacter {
+    var service: \(module)ServiceInputProtocol?
+
+    var output: \(module)InteractorOutputProtocol?
+
+}
+
+//MARK: - \(module)UseCase
+extension \(module)Interacter: \(module)UseCase {
+
+}
+
+//MARK: - \(module)ServiceOutputProtocol
+extension \(module)Interacter: \(module)ServiceOutputProtocol {
+
+    func onNoNetwork() {
+
+    }
+
+    func onErrorOccur(with error: String?) {
+
+    }
 
 }
 
@@ -144,7 +189,7 @@ let presenterFileContent = """
 
 import Foundation
 
-class \(module)Presenter: \(module)Presentation {
+class \(module)Presenter {
 
     weak var view: \(module)View?
 
@@ -153,6 +198,25 @@ class \(module)Presenter: \(module)Presentation {
     var interactor: \(module)UseCase?
 
 }
+
+//MARK: - \(module)Presentation
+extension \(module)Presenter: \(module)Presentation {
+
+}
+
+//MARK: - \(module)InteractorOutputProtocol
+extension \(module)Presenter: \(module)InteractorOutputProtocol {
+
+    func didNoNetwork() {
+
+    }
+
+    func didErrorOccur(with error: String?) {
+
+    }
+
+}
+
 
 """
 
@@ -171,6 +235,7 @@ class \(module)Router: \(module)WireFrame {
         let presenter = \(module)Presenter()
         let router = \(module)Router()
         let interacter = \(module)Interacter()
+        let service = \(module)Service()
 
         view.presenter = presenter
 
@@ -180,6 +245,11 @@ class \(module)Router: \(module)WireFrame {
 
         router.viewController = view
 
+        interacter.output = presenter
+        interacter.service = service
+
+        service.requestHandler = interacter
+
         return view
     }
 
@@ -187,10 +257,63 @@ class \(module)Router: \(module)WireFrame {
 
 """
 
+//MARK: Endpoint File content
+let endPointFileContent = """
+\(fileComment(for: module, type: "Endpoint"))
+
+import Foundation
+
+enum \(module)Endpoint {
+
+}
+
+extension \(module)Endpoint: EndPointType {
+
+    var path: String {
+        return ""
+    }
+
+    var httpMethod: HTTPMethod {
+        return .get
+        }
+
+    var task: HTTPTask {
+        return .request
+    }
+
+    var headers: HTTPHeaders? {
+        return nil
+    }
+
+    var body: Parameters? {
+        return nil
+    }
+
+    var urlParams: Parameters? {
+        return nil
+    }
+
+}
+
+"""
+
+//MARK: Service File content
+let serviceFileContent = """
+\(fileComment(for: module, type: "Service"))
+
+import Foundation
+
+class \(module)Service: \(module)ServiceInputProtocol {
+    weak var requestHandler: \(module)ServiceOutputProtocol?
+}
+
+"""
+
+
 //MARK: Write file
 
 do {
-    try [moduleUrl, contractUrl, viewUrl, interactorUrl, presenterUrl, routerUrl].forEach {
+    try [moduleUrl, commonURL, contractUrl, viewUrl, interactorUrl, presenterUrl, routerUrl, serviceURL].forEach {
         try fileManager.createDirectory(at: $0, withIntermediateDirectories: true, attributes: nil)
     }
     
@@ -199,6 +322,8 @@ do {
     try interactorFileContent.write(to: interactorFileUrl, atomically: true, encoding: .utf8)
     try presenterFileContent.write(to: presenterFileUrl, atomically: true, encoding: .utf8)
     try routerFileContent.write(to: routerFileUrl, atomically: true, encoding: .utf8)
+    try endPointFileContent.write(to: endpointFileURL, atomically: true, encoding: .utf8)
+    try serviceFileContent.write(to: serviceFileURL, atomically: true, encoding: .utf8)
     
 } catch {
     print(error.localizedDescription)
